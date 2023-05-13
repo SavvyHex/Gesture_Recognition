2cd805d74e3d8245dce34643f3170c19a1a433d8
SavvyHex <paisaketh@gmail.com>
2023-05-13 15:40:13 +0530

    started command creation

--- src/admin.py
+++ src/admin.py
@@ -1,7 +1,18 @@
 import tkinter
+import tkinter.messagebox
+import mysql.connector
 
 class AdminScreen:
     def __init__(self) -> None:
+        self.connection = mysql.connector.connect(user="saketh", password="notpassword", host="localhost", database="smartges")
+        self.cursor = self.connection.cursor()
+    
+        self.thumb = tkinter.BooleanVar()
+        self.index = tkinter.BooleanVar()
+        self.middle = tkinter.BooleanVar()
+        self.ring = tkinter.BooleanVar()
+        self.pinky = tkinter.BooleanVar()
+
         self.colours = {"eggplant" : "#6C464F", 
                         "mb pink" : "#9E768F", 
                         "cool gray" : "#9FA4C4", 
@@ -13,12 +24,46 @@ class AdminScreen:
         self.root.geometry("640x480")
         self.root.configure(bg=self.colours["cool gray"])
         
+        self.labels()
+        self.textbox()
         self.buttons()
         
         self.root.mainloop()
         
+    def labels(self):
+        self.title = tkinter.Label(self.root, text="Command Creation", bg=self.colours["cool gray"], font=("Helvetica", 28))
+        self.title.place(x=150, y=50)
+
+        self.file_name = tkinter.Label(self.root, text="File Name : ", bg=self.colours["cool gray"], font=("Helvetica", 18))
+        self.file_name.place(x=10, y=130)
+
+        self.checkbox_title = tkinter.Label(self.root, text="Check the boxes for the respective finger to be opened", bg=self.colours["cool gray"], font=("Helvetica", 14))
+        self.checkbox_title.place(x=40, y=180)
+
+    def textbox(self):
+        self.fileName = tkinter.Entry(self.root)
+        self.fileName.place(x=150, y=138)
+
     def buttons(self):
-        self.submit = tkinter.Button(self.root, text="Submit").place(x=260, y=400)
+        self.submit = tkinter.Button(self.root, text="Submit", command=self.submit)
+        self.submit.place(x=300, y=400)
+
+        self.thumbButton = tkinter.Checkbutton(self.root, text="Thumb", bg=self.colours["cool gray"], font=("Helvetica", 16), variable=self.thumb).place(x=50, y=220)
+        self.indexButton = tkinter.Checkbutton(self.root, text="Index", bg=self.colours["cool gray"], font=("Helvetica", 16), variable=self.index).place(x=50, y=260)
+        self.middleButton = tkinter.Checkbutton(self.root, text="Middle", bg=self.colours["cool gray"], font=("Helvetica", 16), variable=self.middle).place(x=50, y=300)
+        self.ringButton = tkinter.Checkbutton(self.root, text="Ring", bg=self.colours["cool gray"], font=("Helvetica", 16), variable=self.ring).place(x=50, y=340)
+        self.pinkyButton = tkinter.Checkbutton(self.root, text="Pinky", bg=self.colours["cool gray"], font=("Helvetica", 16), variable=self.pinky).place(x=50, y=380)
+
+    def submit(self):
+        self.code = f"{1 if self.thumb else 0}{1 if self.index else 0}{1 if self.middle else 0}{1 if self.ring else 0}{1 if self.pinky else 0}"
+
+        self.file = self.fileName.get()
+        self.cursor.execute(f"insert into gestures values({self.code}, {self.file})")
+
+        self.connection.commit()
+        self.connection.close()
+        self.root.destroy()
+        tkinter.messagebox.Message(self.root, message="Database Successfully Updated")
         
 if __name__ == "__main__":
     AdminScreen()
--- src/command_controller.py
+++ src/command_controller.py
@@ -2,10 +2,19 @@ import cv2
 import os
 import subprocess
 
+import mysql.connector
+
 import hand_tracker as ht
 
 class CommandController:
     def __init__(self) -> None:
+        self.connection = mysql.connector.connect(user="saketh", password="notpassword", host="localhost", database="smartges")
+        self.cursor = self.connection.cursor()
+
+        self.cursor.execute("select * from gestures")
+        self.commands = self.cursor.fetchall()
+        self.connection.close()
+
         self.system = os.name
         self.tracker = ht.HandTracker(maxHands=1)
         self.fingers = list()
@@ -27,13 +36,18 @@ class CommandController:
     
     def execute_command(self):
         self.get_open_fingers()
-        
-        if self.fingers == [0, 1, 0, 0, 1]:
-            if self.system.lower() == "nt":
-                proccess = subprocess.Popen(["C:/Users/neetu\Desktop/Saketh's Stuff/Gesture_Recognition/src/commands/command1.bat"])
-                proccess.wait()
-            else:
-                subprocess.call(["sh", "src/commands/command1.sh"])
+
+        for cmd in self.commands:
+            code = ''.join(str(i) for i in self.fingers)
+            if code == cmd[0]:
+                try:
+                    if self.system.lower() == "nt":
+                        proccess = subprocess.Popen([f"C:/Users/neetu/Desktop/Saketh's Stuff/Gesture_Recognition/src/commands/{cmd[1]}.bat"])
+                        proccess.wait()
+                    else:
+                        subprocess.call(["sh", f"src/commands/{cmd[1]}.sh"])
+                except:
+                    print("file not found")
     
     def run(self):
         cap = cv2.VideoCapture(1)
@@ -42,10 +56,11 @@ class CommandController:
             img = self.tracker.handsFinder(img)
             self.lmList = self.tracker.positionFinder(img)
             cv2.imshow("Video",img)
-            if cv2.waitKey(5) & 0xFF == ord('q'):
+            if cv2.waitKey(1) & 0xFF == ord('q'):
                 cap.release()
-            self.execute_command()
+                break
             cv2.waitKey(1)
+            self.execute_command()
             
 if __name__ == "__main__":
     cc = CommandController()
