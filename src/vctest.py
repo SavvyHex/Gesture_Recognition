8c4a309b3b02751aaaac42f5d1cb1f11f00f6a09
antiiiiny <131854144+antiiiiny@users.noreply.github.com>
2023-05-21 20:37:06 +0530

    vctest.py
    read the comments

--- src/vctest.py
+++ src/vctest.py
@@ -20,20 +20,17 @@ devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(
     IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
-# volume.GetMute()
-# volume.GetMasterVolumeLevel()
 volRange = volume.GetVolumeRange()
 minVol = volRange[0]
 maxVol = volRange[1]
 vol = 0
-volBar = 400
+volBar = 400 
 volPer = 0
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
     if len(lmList) != 0:
-        # print(lmList[4], lmList[8])
 
         x1, y1 = lmList[4][1], lmList[4][2]
         x2, y2 = lmList[8][1], lmList[8][2]
@@ -45,31 +42,29 @@ while True:
         cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
 
         length = math.hypot(x2 - x1, y2 - y1)
-        # print(length)
 
-        # Hand range 50 - 300
-        # Volume Range -65 - 0
+        # Hand range we gotta set this
+        # Volume Range gotta find this
+        #these values gonna change based on whose hand is gonna be used
 
-        vol = np.interp(length, [50, 300], [minVol, maxVol])
-        volBar = np.interp(length, [50, 300], [400, 150])
-        volPer = np.interp(length, [50, 300], [0, 100])
-        print(int(length), vol)
-        volume.SetMasterVolumeLevel(vol, None)
+        #vol = np.interp(length, [50, 300], [minVol, maxVol])
+        #volBar = np.interp(length, [50, 300], [400, 150])
+        #volPer = np.interp(length, [50, 300], [0, 100])
+        #print(int(length), vol)
+        #volume.SetMasterVolumeLevel(vol, None)
 
         if length < 50:
-            cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+            cv2.circle(img, (cx, cy), 15, (255, 255, 255), cv2.FILLED)
 
-    cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
-    cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
-    cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX,
-                1, (255, 0, 0), 3)
+    cv2.rectangle(img, (50, 150), (85, 400), (255, 255, 255), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 255, 255), cv2.FILLED)
+    cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 3)
 
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX,
-                1, (255, 0, 0), 3)
+    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX,1, (255, 255, 255), 3)
 
     cv2.imshow("Img", img)
     cv2.waitKey(1)
